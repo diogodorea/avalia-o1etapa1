cd0deb9aa31ff6e22d6a7c7c1fc1f9434724e159
Diogo Dorea <diogodorea@gmail.com>
2023-10-24 00:28:53 -0300

    definição da classe paciente

--- main.cpp
+++ main.cpp
@@ -1,6 +1,60 @@
 #include<iostream>
+#include <vector>
+#include <string>
 
-usingnamespace std;
+using namespace std;
+
+class Paciente {
+private:
+    string cpf;
+    string dtNascimento;
+
+public:
+    string nome;
+
+    Paciente() {
+        nome = "Vazio";
+        cpf = "Vazio";
+        dtNascimento = "Vazio";
+    }
+
+    Paciente(string _nome, string _cpf, string _dtNascimento) {
+        this->nome = _nome;
+        this->setCpf(_cpf);
+        this->setDtNascimento(_dtNascimento);
+    }
+
+    string getCpf() {
+        return this->cpf;
+    }
+
+    string getDtNascimento() {
+        return this->dtNascimento;
+    }
+
+    string getNome() {
+        return this->nome;
+    }
+
+    
+    void setCpf(string _cpf) {
+        this->cpf = _cpf;
+    }
+        
+    void setDtNascimento(string _data_nascimento) {
+        this->dtNascimento = _data_nascimento;
+    }
+
+    void setNome(string _nome) {
+        this->nome = _nome;
+    }
+
+    void imprimir(){
+        cout << "Nome: "<< nome << endl;
+        cout <<  "CPF: " << cpf << endl;
+        cout <<  "Nascimento: " << dtNascimento << endl;
+    }
+};
 
 int main(){
   return 0;
